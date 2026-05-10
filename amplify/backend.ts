8f2f915457714a4db6ebfecb6d2ee8ac6c5866bb
rcarvajalp <166424809+rcarvajalp@users.noreply.github.com>
2026-05-09 20:39:29 -0600

    Update hero subtitle

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,12 +2,18 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { storage, secondaryStorage } from './storage/resource';
 
-
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
-  storage, 
-  secondaryStorage
+  storage,
+  secondaryStorage,
 });
+
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+
+cfnUserPool.adminCreateUserConfig = {
+  ...cfnUserPool.adminCreateUserConfig,
+  allowAdminCreateUserOnly: true,
+};
