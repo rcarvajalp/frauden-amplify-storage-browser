47fef5c40b7bb22146717d17b44b6ff740fcecc5
rcarvajalp <rigocp2002@gmail.com>
2026-05-11 20:14:45 -0600

    Revert "Merge pull request #4 from rcarvajalp/codex/locate-buckets-without-prefixes-or-suffixes"
    This reverts commit 2b5b6eb3af63cba188a2c9021730ffe313625bf5, reversing
    changes made to 512a08da91d5b9c6c787fc4ebb3bb71ba97d94bd.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import {
-  secondaryStorage,
-  secondaryStorageBucketName,
-  storage,
-  storageBucketName,
-} from './storage/resource';
+import { storage, secondaryStorage } from './storage/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
