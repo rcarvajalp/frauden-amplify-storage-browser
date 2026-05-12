7fa26ae5567371ab55dc8bee63af7a81b89a107e
rcarvajalp <rigocp2002@gmail.com>
2026-05-11 20:19:38 -0600

    quitar la palabra clave bucket del nombre del storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,7 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'frauden-bucket',
+  name: 'frauden',
   isDefault: true,
   access: (allow) => ({
     'doctrina/*': [
