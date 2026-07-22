ff5b06f18814bc8c78251a800a2b3ad852a4a6e0
rcarvajalp <rigocp2002@gmail.com>
2026-07-22 03:38:23 -0600

    Update Amplify branch linker runtime to Node.js 24

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { Aspects, IAspect } from 'aws-cdk-lib';
+import { CfnFunction } from 'aws-cdk-lib/aws-lambda';
+import { IConstruct } from 'constructs';
 import { auth } from './auth/resource';
-import { storage, secondaryStorage } from './storage/resource';
+import { secondaryStorage, storage } from './storage/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -13,6 +16,16 @@ const backend = defineBackend({
 
 // Do not override cfnBucket.bucketName here. S3 bucket names are globally unique,
 // and Amplify's generated physical names avoid collisions during deployments.
+
+class AmplifyBranchLinkerNodeRuntime implements IAspect {
+  visit(node: IConstruct) {
+    if (node instanceof CfnFunction && node.node.path.includes('AmplifyBranchLinker')) {
+      node.runtime = 'nodejs24.x';
+    }
+  }
+}
+
+Aspects.of(backend.stack).add(new AmplifyBranchLinkerNodeRuntime());
 
 const { cfnUserPool } = backend.auth.resources.cfnResources;
 
